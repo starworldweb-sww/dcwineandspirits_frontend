bf37e432f94e4d9503b360727ffa07d80039ab1f
Deepak Rai <dpk.developer04@gmail.com>
2026-09-01 19:43:47 +0530

    add some changes in blog

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -4,18 +4,46 @@ import { getQueryClient } from '@/libs/get-query-client';
 import { blogService } from '../api/services/blogService';
 import { blogKeys } from '@/libs/queryKeys';
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { getMetaByType } from '@/libs/getMetaByType';
 
 
-export const metadata = {
-  title: "Wine & Spirits Blogs | Expert Advice, Pairing Tips & Gift Ideas",
-  description:
-    "Discover expert wine and spirits advice, food pairing tips, and creative celebration ideas on the DC Wine and Spirits blog. Read our latest articles and guides.",
-  alternates: {
-    canonical: "https://www.dcwineandspirits.com/blogs/",
-  },
-};
+// export const metadata = {
+//   title: "Wine & Spirits Blogs | Expert Advice, Pairing Tips & Gift Ideas",
+//   description:
+//     "Discover expert wine and spirits advice, food pairing tips, and creative celebration ideas on the DC Wine and Spirits blog. Read our latest articles and guides.",
+//   alternates: {
+//     canonical: "https://www.dcwineandspirits.com/blogs/",
+//   },
+// };
 
 
+export async function generateMetadata({ params }) {
+  const { slug } = await params;
+ 
+  // const matchedCategory = allCategories?.find((c) => c.slug === slug);
+  const meta = await getMetaByType("blog", slug)
+  console.log("meta", meta)
+  if (meta) {
+    return {
+      title: `${meta?.meta_title} Blogs | DC Wine and Spirits`,
+      description:
+        meta.meta_description ||
+        `Browse all blogs in the ${meta.meta_description} category on DC Wine and Spirits.`,
+      alternates: {
+        canonical: `https://www.dcwineandspirits.com/blogs/${slug}`,
+      },
+    };
+  }
+
+  return {
+    title: "Wine & Spirits Blogs | Expert Advice, Pairing Tips & Gift Ideas",
+    description:
+      "Discover expert wine and spirits advice, food pairing tips, and creative celebration ideas on the DC Wine and Spirits blog. Read our latest articles and guides.",
+    alternates: {
+      canonical: `https://www.dcwineandspirits.com/blogs/${slug}`,
+    },
+  };
+}
 const page = async () => {
 
   const queryClient = getQueryClient();
@@ -23,7 +51,7 @@ const page = async () => {
 
   await queryClient.prefetchQuery({
     queryKey: blogKeys.posts({ page: 1, limit: 10 }),
-    queryFn: () => Promise.resolve(initialData), 
+    queryFn: () => Promise.resolve(initialData),
   });
 
 
