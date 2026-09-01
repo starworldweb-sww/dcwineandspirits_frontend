57da2ee8024d013f8acf7331dc8737cd1b29375d
Aditya Pathak <pathakaditya270@gmail.com>
2026-09-01 18:50:17 +0530

    privacy metadata

--- src/app/privacy/page.jsx
+++ src/app/privacy/page.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import PrivacyClient from './PrivacyClient'
 
+
+
+
+export const metadata = {
+  title: "Privacy Policy | DC Wine & Spirits",
+  description:
+    "Dcwineandspirits.com respects each individual user's right to privacy. We take decent precautions to keep the personal information disclosed to us secure.",
+  alternates: {
+    canonical: "https://www.dcwineandspirits.com/privacy/",
+  },
+};
+
 const page = () => {
   return (
     <>
